59615810430c0d41948ab59fed87fe8d7e74eb79
Natarajan <umamags@gmail.com>
2021-03-16 11:36:19 +0530

    components

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h2>Homepage v2</h2>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>The Dojo Blog</h1>
+      <div clasName="links">
+        <a href="/">Home</a>
+        <a href="/create">New Blog</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
