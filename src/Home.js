252c1a1b8034503c05d764b9cad0cea1ff3ee0b1
Natarajan <umamags@gmail.com>
2021-03-19 10:56:12 +0530

    delete

--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,21 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
-  const [name, setName] = useState('mahesh');
-  const [age, setAge] = useState(20);
+  const [blogs, setBlogs] = useState([
+   { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+   { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+   { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+ ]);
 
-  const handleClick = () => {
-    setName('abhinav');
-    setAge(30);
-    console.log('name set to abhinav');
-  }
-  const handleClickAgain = (name, e) => {
-    console.log('hello ' + name, e.target);
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
   }
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>{name} is {age} years old</p>
-      <button onClick={handleClick}>Click Me</button>
-      <button onClick={() => {
-        handleClickAgain('Mahesh');
-      }}>Click Me Again</button>
-      <button onClick={(e) => handleClickAgain('Mahesh Again', e)}>Click Me Third Time</button>
+      <BlogList blogs={blogs} handleDelete={handleDelete} title="All Blogs"/>
+      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} handleDelete={handleDelete} title="Mario Blogs"/>
     </div>
   );
 }
