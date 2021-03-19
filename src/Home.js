4df41c061666050ef841f890706fd5c3262d80a1
Natarajan <umamags@gmail.com>
2021-03-19 12:17:43 +0530

    use effect

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -8,14 +8,23 @@ const Home = () => {
    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
  ]);
 
+ const [name, setName] = useState('mario');
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
   }
+
+  useEffect(() => {
+    console.log("Use effect ran");
+  }, [name]);
+
   return (
     <div className="home">
       <BlogList blogs={blogs} handleDelete={handleDelete} title="All Blogs"/>
       <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} handleDelete={handleDelete} title="Mario Blogs"/>
+      <button onClick={() => setName('luigi')}>change name</button>
+      <p>{ name }</p>
     </div>
   );
 }
