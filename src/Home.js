9560f152e0cf2c130f6a69579b3a33a5ca73f734
Natarajan <umamags@gmail.com>
2021-03-19 12:50:51 +0530

    added rest call

--- src/Home.js
+++ src/Home.js
@@ -2,11 +2,7 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-   { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-   { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-   { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
- ]);
+  const [blogs, setBlogs] = useState(null);
 
  const [name, setName] = useState('mario');
 
@@ -16,13 +12,19 @@ const Home = () => {
   }
 
   useEffect(() => {
-    console.log("Use effect ran");
+    fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setBlogs(data);
+      })
   }, [name]);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} handleDelete={handleDelete} title="All Blogs"/>
-      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} handleDelete={handleDelete} title="Mario Blogs"/>
+      {blogs && <BlogList blogs={blogs} handleDelete={handleDelete} title="All Blogs"/>}
+      {blogs && <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} handleDelete={handleDelete} title="Mario Blogs"/>}
       <button onClick={() => setName('luigi')}>change name</button>
       <p>{ name }</p>
     </div>
