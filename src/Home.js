08119c89b5d2745feec31425a03157a67292357d
Natarajan <umamags@gmail.com>
2021-03-20 16:25:48 +0530

    error added

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,8 @@ const Home = () => {
   const [blogs, setBlogs] = useState(null);
 
  const [name, setName] = useState('mario');
+ const [isPending, setPending] = useState(true);
+ const [error, setError] = useState(null);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -12,17 +14,31 @@ const Home = () => {
   }
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        setBlogs(data);
-      })
-  }, [name]);
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+        .then(res => {
+          if (!res.ok) {
+            throw Error("could not fetch the data");
+          }
+          return res.json();
+        })
+        .then(data => {
+          setBlogs(data);
+          setPending(false);
+          setError(null);
+        })
+        .catch(err => {
+          console.log(err.message);
+          setPending(false);
+          setError(err.message);
+        })
+    }, 1000);
+  }, []);
 
   return (
     <div className="home">
+      {error && <div> {error} </div>}
+      {isPending && <div>Loading..</div>}
       {blogs && <BlogList blogs={blogs} handleDelete={handleDelete} title="All Blogs"/>}
       {blogs && <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} handleDelete={handleDelete} title="Mario Blogs"/>}
       <button onClick={() => setName('luigi')}>change name</button>
