804926dad7216792ede359ca10be3b5473a93f97
Natarajan <umamags@gmail.com>
2021-03-21 11:51:58 +0530

    form submission and post

--- src/Create.js
+++ src/Create.js
@@ -4,10 +4,21 @@ const Create = () => {
   const [title, setTitle] = useState('Hello');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
-  const handleSubmit = (e) => {      
+  const [isPending, setIsPending] = useState(false);
+
+  const handleSubmit = (e) => {
       e.preventDefault();
       const blog = { title, body, author};
-      console.log(blog);
+
+      setIsPending(true);
+      fetch('http://localhost:8000/blogs', {
+        method: 'POST',
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(blog)
+      }).then(() => {
+        setIsPending(false);
+        console.log("New blog added");
+      })
   }
   return (
     <div className="create">
@@ -33,7 +44,9 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add Blog</button>
+        { !isPending && <button>Add Blog</button>}
+        { isPending && <button disabled>Blog Added..</button>}
+
         </form>
         <p>{ title }</p>
         <p>{ body }</p>
